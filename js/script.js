ed73a262ac8a63d7f6e8d54f76bc00d8f1096249
kevin <kevin.e@qq.com>
2017-01-13 18:32:35 +0800

    lint

--- js/script.js
+++ js/script.js
@@ -1,126 +1,131 @@
-window.onload=function () {
-     btnEvent();
-}
-//全局注册按钮事件
+window.onload = function() {
+        btnEvent();
+    }
+    //全局注册按钮事件
 function btnEvent() {
-    var  body = document.getElementById("body");
-    eventUtil.addHandler(body,"click",function (e) {
+    var body = document.getElementById("body");
+    eventUtil.addHandler(body, "click", function(e) {
         var event = eventUtil.getEvent(e);
         var target = eventUtil.getElement(event);
         console.log(target.nodeName);
-        switch(target.id){
-            case "closeReg":closeReg();
-            break;
-            case "logoff":logoff();
-            break;
-            case "regist":regist();
-            break;
-            case "modify":modify();
-            break;
-            default :console.log("无事件..");
+        switch (target.id) {
+            case "closeReg":
+                closeReg();
+                break;
+            case "logoff":
+                logoff();
+                break;
+            case "regist":
+                regist();
+                break;
+            case "modify":
+                modify();
+                break;
+            default:
+                console.log("无事件..");
         }
     });
 }
 
 //打开注册面板
 function regist() {
-    document.getElementById("reg-panel").style.display="block";
+    document.getElementById("reg-panel").style.display = "block";
     showModel();
 }
 //退出系统
-function logoff(){
+function logoff() {
     console.log("退出");
 }
 
 //修改信息
 function modify() {
     console.log("设置");
-    loadAjaxData("./request.txt","")
+    loadAjaxData("./request.txt", "")
 }
 
 //关闭注册面板
 function closeReg() {
     // var close =  document.getElementById("closeReg");
     // eventUtil.addHandler(close,"click",function() {
-        document.getElementById("reg-panel").style.display="none";
-        hideModel();
+    document.getElementById("reg-panel").style.display = "none";
+    hideModel();
     // })
 }
 
 //控制模态框 显示/隐藏
-function showModel(){
-    document.getElementById("modal").style.display="block";
+function showModel() {
+    document.getElementById("modal").style.display = "block";
 }
-function hideModel(){
-    document.getElementById("modal").style.display="none";
+
+function hideModel() {
+    document.getElementById("modal").style.display = "none";
 }
 
 //ajax 请求数据
-function loadAjaxData(url)
-{
-  var xmlhttp;
-  if (window.XMLHttpRequest){
-        xmlhttp=new XMLHttpRequest();
-    }else{
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+function loadAjaxData(url) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-  xmlhttp.onreadystatechange=function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200){
-            console.log(xmlhttp.responseText);//得到数据
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log(xmlhttp.responseText); //得到数据
         }
-  }
-  xmlhttp.open("GET",url,true);
-//   xmlhttp.setRequestHeader("Content-type","text/plain;charset=UTF-8");
-  xmlhttp.send();//参数"fname=Henry&lname=Ford"
+    }
+    xmlhttp.open("GET", url, true);
+    //   xmlhttp.setRequestHeader("Content-type","text/plain;charset=UTF-8");
+    xmlhttp.send(); //参数"fname=Henry&lname=Ford"
 }
 
 //事件通用工具
 var eventUtil = {
     //添加句柄
-    addHandler:function (element,type,handler) {
-        if(element.addEventListener){
-            element.addEventListener(type,handler,false);
-        }else if(element.attachEvent){
-            element.attachEvent('on' + type,handler);
-        }else {
+    addHandler: function(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + type, handler);
+        } else {
             element['on' + type] = handler;
         }
     },
     //删除句柄
-    removeHandler:function (element,type,handler) {
-        if(element.removeEventListener){
-            element.removeEventListener(type,handler,false);
-        }else if(element.detachEvent){
-            element.detachEvent('on' + type,handler);
-        }else {
+    removeHandler: function(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent('on' + type, handler);
+        } else {
             element['on' + type] = null;
         }
     },
     //获得事件对象
-    getEvent:function (event) {
-        return event?event:window.event;
+    getEvent: function(event) {
+        return event ? event : window.event;
     },
     //获得事件类型
-    getType:function (event) {
+    getType: function(event) {
         return event.type;
     },
     //获得目标对象
-    getElement:function (event) {
+    getElement: function(event) {
         return event.target || event.srcElement;
     },
     //阻止元素默认行为
-    preventDefault:function (event) {
-        if(event.preventDefault){
+    preventDefault: function(event) {
+        if (event.preventDefault) {
             event.preventDefault();
-        }else{
+        } else {
             event.returnValue = false;
         }
     },
     //阻止冒泡
-    stopPropagation:function (event) {
-        if(event.stopPropagation){
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
             event.stopPropagation();
-        }else{
+        } else {
             event.cancelBubble = true;
         }
     }
